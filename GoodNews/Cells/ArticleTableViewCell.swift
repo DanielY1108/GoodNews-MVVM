9dd4aa3bdcd653fc9a40122eea3008d3c984c111
DanielY1108 <scarlet040@gmail.com>
2023-02-05 22:10:36 +0900

    tableViewCell 속성 연결 및 레이아웃 설정

---
+++ GoodNews/Cells/ArticleTableViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  ArticleTableViewCell.swift
+//  GoodNews
+//
+//  Created by JINSEOK on 2023/02/05.
+//
+
+import UIKit
+
+class ArticleTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+}
